c5c47dbcc1a8d572b93702fda9b5d9103065a75d
jihuder <julioarenas85@gmail.com>
2022-01-14 20:24:17 -0500

    symbolic link

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-# sets up nginx web server and the host 
-apt-get update
-apt-get install nginx -y 
-mkdir -p /data/web_static/shared
-mkdir -p /data/web_static/releases/test/
-echo "Holberton School" >> /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test /data/web_static/current
-chown -R ubuntu:ubuntu /data/
-sed -i "40i \\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n" /etc/nginx/sites-available/default
+# sets up nginx web server and the host
+sudo apt-get update
+sudo apt-get install nginx -y
+sudo mkdir -p /data/web_static/shared
+sudo mkdir -p /data/web_static/releases/test/
+echo "<h1>Holberton School</h1>" > ~/index.html
+sudo cp ~/index.html /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/index.html  /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i "40i \\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n" /etc/nginx/sites-available/default
 sudo service nginx restart
