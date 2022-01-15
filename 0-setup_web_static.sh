0ae0a8d45efe620ff880f7a503a010e3dc367782
jihuder <julioarenas85@gmail.com>
2022-01-14 20:28:18 -0500

    symbolic link

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,7 +6,7 @@ sudo mkdir -p /data/web_static/shared
 sudo mkdir -p /data/web_static/releases/test/
 echo "<h1>Holberton School</h1>" > ~/index.html
 sudo cp ~/index.html /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/index.html  /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/  /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "40i \\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n" /etc/nginx/sites-available/default
 sudo service nginx restart
